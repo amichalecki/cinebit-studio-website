55a8f2088d9996f81a1cd72ad1114e6477ad75d5
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2020-01-21 21:32:27 +0100

    photo modals

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) { myIndex = 1 }
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 3000);
+    setTimeout(carousel, 4000);
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
@@ -25,9 +25,22 @@ function myFunction() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('photoModal');
+var modal = document.getElementById('portfolio1');
+var modal2 = document.getElementById('portfolio2');
+var modal3 = document.getElementById('portfolio3');
+var modal4 = document.getElementById('portfolio4');
+
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    if (event.target == modal4) {
+        modal4.style.display = "none";
     }
 }
